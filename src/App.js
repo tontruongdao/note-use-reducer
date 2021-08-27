3dfc41ae1ef9482035ee633ec74046e5cf192fbe
truongsinistar <truong@sinistar.ca>
2021-08-27 16:14:46 -0400

    more boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         useReducer
+        <div>{count}</div>
+        <div style={{ margin: "20px", width: "30vw" }}>
+          <button style={{ width: "5vw"}} onClick={decrement}>-</button>
+          <button style={{ width: "5vw"}} onClick={increment}>+</button>
+        </div>
       </header>
     </div>
   );
