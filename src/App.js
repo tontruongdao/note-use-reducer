c52f0f667626374b3e0965db1d822f48f93e9b30
truongsinistar <truong@sinistar.ca>
2021-08-27 16:54:51 -0400

    refactored actions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { useReducer } from 'react'
 
 import './App.css';
 
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'increment':
+    case ACTIONS.INCREMENT:
       return { count: state.count + 1 };
-    case 'decrement':
+    case ACTIONS.DECREMENT:
       return { count: state.count - 1 };
     default:
       return state;
@@ -17,11 +22,11 @@ function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 })
 
   const increment = () => {
-    dispatch({ type: 'increment' })
+    dispatch({ type: ACTIONS.INCREMENT })
   }
 
   const decrement = () => {
-    dispatch({ type: 'decrement' })
+    dispatch({ type: ACTIONS.DECREMENT })
   }
 
   return (
