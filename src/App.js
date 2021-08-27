a927e644ca0bdf9c1d1e9d2e1e82db92a7a19596
truongsinistar <truong@sinistar.ca>
2021-08-27 16:44:39 -0400

    added dispatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 
 import './App.css';
 
+const reducer = (state, action) => {
+  return { count: state.count + 1 }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
 
   const increment = () => {
-    setCount(prevCount => prevCount + 1)
+    dispatch()
   }
 
-  const decrement = () => {
-    setCount(prevCount => prevCount - 1)
-  }
+  // const decrement = () => {
+  //   setCount(prevCount => prevCount - 1)
+  // }
 
   return (
     <div className="App">
       <header className="App-header">
-        useReducer
-        <div>{count}</div>
+        <h3>useReducer</h3>
+        <div>{state.count}</div>
         <div style={{ margin: "20px", width: "30vw" }}>
-          <button style={{ width: "5vw"}} onClick={decrement}>-</button>
+          {/* <button style={{ width: "5vw"}} onClick={decrement}>-</button> */}
           <button style={{ width: "5vw"}} onClick={increment}>+</button>
         </div>
       </header>
